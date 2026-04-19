dddf5483fc78ebaaa3ab2a5af84b1039fb82bc8c
Mr. Suvijoy Das <sdas014984@gmail.com>
2026-04-19 13:38:28 +0530

    Update backend.tf

--- backend/backend.tf
+++ backend/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "enterprise-tf-state"
-    key            = "landingzone/terraform.tfstate"
+    bucket         = "my-terraform-state-bucket-32123 "
+    key            = "dev/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "tf-locks"
     encrypt        = true
