83391e29eff56132f4ccdf74369a7723a10cb662
Mr. Suvijoy Das <sdas014984@gmail.com>
2026-04-19 13:39:34 +0530

    Configure S3 backend for Terraform state

--- workloads/dev/backend.tf
+++ workloads/dev/backend.tf
@@ -1,1 +1,10 @@
 
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket-32123 "
+    key            = "dev/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "tf-locks"
+    encrypt        = true
+  }
+}
