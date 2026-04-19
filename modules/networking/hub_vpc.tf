d17e15254bb86e4a98d547c8d58e50af380013a0
sdas014984 <sdas014984@gmail.com>
2026-04-19 14:10:57 +0530

    updated into modules

--- modules/networking/hub_vpc.tf
+++ modules/networking/hub_vpc.tf
@@ -7,8 +7,3 @@ module "hub_vpc" {
   public_subnets  = ["10.0.1.0/24"]
   private_subnets = ["10.0.2.0/24"]
 }
-resource "aws_vpc_endpoint" "ssm" {
-  vpc_id            = var.vpc_id
-  service_name      = "com.amazonaws.ap-south-1.ssm"
-  vpc_endpoint_type = "Interface"
-}
